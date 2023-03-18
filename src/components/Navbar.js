d850ba5a761149266781488cf2e2c1b9aca95f72
unknown <davidstew87@gmail.com>
2023-03-18 16:47:27 -0400

    worked on homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react';
+
 import { Link } from "react-router-dom"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Nav';
 import Col from 'react-bootstrap/Navbar';
-import image from '../images/mtgHeader.png'
+import image from '../images/mtgHeader.png';
+
 
 const styles = {
   name: {
    fontSize: '95px',
    fontFamily: 'palatino',
-   color: 'grey',
-   textShadow: '2px 2px 3px white',
+   color: '#108B30',
+   textShadow: '2px 2px 3px blue',
    display: 'flex',
    justifyContent:'center'
-
   },
   navbar: {
     display: 'flex',
@@ -21,7 +22,7 @@ const styles = {
     backgroundImage: "url(" + image + ")",
     backgroundSize: 'contain',
     // background: 'linear-gradient(45deg,red,silver,black,green,blue)',
-    border: 'solid 7.5px grey',
+    // border: 'solid 7.5px grey',
     width: 'auto'
   },
   container: {
@@ -34,9 +35,9 @@ const styles = {
   tabItems: {
     display: 'flex',
     fontSize: '37.5px',
-    color: 'white',
+    color: '#108B30',
     padding: '5px',
-    textShadow: '2px 2px 3px black',
+    textShadow: '2px 2px 3px blue',
     textDecoration: 'none',
     height: '100%',
   
@@ -59,23 +60,35 @@ const styles = {
 function HomeBar() {
   return (
     <>
-     <Container fluid style={styles.container}>
-       <Row bg="dark" style={styles.navbar}>
+      <Container fluid style={styles.container}>
+        <Row bg="dark" style={styles.navbar}>
           <Col md={6} style={styles.colstyles}>
+            
             <h1 style={styles.name}><center><strong>Magic Builds</strong></center></h1>
           </Col>
           <Col md={6} style={styles.colstyles}>
-           <nav style={styles.tabs} className='nav'> 
-            <ul style={styles.ul}>
-             <li><Link to={"/"} style={styles.tabItems}>Home</Link></li>
-             <li><Link to={"/Sign-in"} style={styles.tabItems}>Sign-in
-             </Link></li>
-             <li><Link to={"/Deck-Builder"} style={styles.tabItems}>Deck Builder</Link></li>
-            </ul>
-           </nav>
+            <nav style={styles.tabs} className="nav">
+              <ul style={styles.ul}>
+                <li>
+                  <Link to={"/"} style={styles.tabItems}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/Sign-in"} style={styles.tabItems}>
+                    Sign-in
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/Deck-Builder"} style={styles.tabItems}>
+                    Deck Builder
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </Col>
-       </Row>
-     </Container>
+        </Row>
+      </Container>
     </>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -5,13 +5,14 @@ import Col from 'react-bootstrap/Col';
 
 const styles = {
   container: {
-    margin: '5px',
-    // background: 'linear-gradient(20deg,dodgerblue,green,silver)',
+    background: 'linear-gradient(20deg,black,white)',
+    padding: '5px'
   },
   colstyles: {
     display: 'flex',
     padding: '25px',
-    justifyContent: 'space-evenly',
+    justifyContent: 'center',
+    alignItems: 'center',
     flexWrap: 'wrap'
   },
   photo: {
@@ -23,6 +24,12 @@ const styles = {
     minHeight: '250px',
     minWidth: '350px'    
   },
+  pIntro: {
+    fontSize: '20px',
+    fontFamily: 'palatino',
+    color: 'black',
+    textShadow: '2px 2px 3px blue',
+  }
 };
 
 // We use JSX curly braces to evaluate the style object on the JSX tag
@@ -31,18 +38,27 @@ function Section() {
   return (
     <Container fluid style={styles.container}>
     <Row>
-       <Col m={4} style={styles.col}>
-       <img src={require('../images/sectionImg1.jpg')} className='img-fluid' alt='planeswalker' style={styles.photo}/>
-       </Col>
-
-       <Col m={4}>
+       <Col lg={4} style={styles.col}>
         <center>
-          <p>Intro to the site placeholder</p>
+          <img src={require('../images/sectionImg1.jpg')} className='img-fluid' alt='planeswalker' style={styles.photo}/>
         </center>
        </Col>
 
-       <Col m={4} style={styles.col}>
+       <Col lg={4}>
+        <center style={styles.pIntro}>
+          <p>Hello and welcome!</p><br></br>
+          <p>I'am building this site to act as a magic the gathering deck builder, This is meant to be a fun tool
+            where one can build, and save deck ideas before constructing them for real. This app will be built using
+            react for the front end and mongo for the backend database, I plan to utilize MTG's open API to pull card
+            information from. 
+          </p>
+        </center>
+       </Col>
+
+       <Col lg={4} style={styles.col}>
+       <center>
        <img src={require('../images/Avacyn.jpg')} className='img-fluid' alt='planeswalker' style={styles.photo}/>
+       </center>
        </Col>
     </Row>
    </Container>
